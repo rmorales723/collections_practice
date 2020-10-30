c90c464d7763421822fd60640cb95a59b7518515
robert morales <rmoralfit@gmail.com>
2020-10-29 20:51:05 -0400

    commit tree 9b804659c2c6a4165af599e514e156f093da6651

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |index|
+  index [2] = "$"
+  end
+end 
+
+def find_a(array)
+  array.find_all do |letter|
+letter[0] == "a"
+  end 
+end
+
+def sum_array(array)
+array.inject(0) do |previous, current|
+previous + current
+end
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
+  
+  
